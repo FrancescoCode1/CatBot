317a386234adee0a958a49993915aea3175048e2
FrancescoCode1 <71893290+FrancescoCode1@users.noreply.github.com>
2020-11-30 18:25:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 # bot.py
 # bot created by Francesco L.C. in association with nkaaf
-import os                               #Cogs allows me to actually keep the main bot tidy
-import discord                          #Im only importing libs needed to run the bot. Inside cogs all the libs are loaded
+# ver 0.5 milestone release
+import discord
+import os
+'''This simply starts the bot. Token is saved in the .env.'''
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()     #Subs to intent
-client = commands.Bot(command_prefix='!', intents=intents)  #cmd prefix is set
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix='!', intents=intents)
 
-load_dotenv()                           #.env file containing token is loaded
+load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')      #Token string is given to TOKEN variable
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-client.load_extension("cogs.MusicCommands") #juicy part. cogs are loaded into the bot
-client.load_extension("cogs.CatCommands")   #more functionalities require more code. with cogs you keep the overview
-
-client.run(TOKEN)                           #token is used to authenticate the bot and actually run it
+client.load_extension("cogs.MusicCommands")
+client.load_extension("cogs.CatCommands")
+client.run(TOKEN)
