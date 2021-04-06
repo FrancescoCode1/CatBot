c0c38ea7cf56c652c39fceceab0ed77f34f4030a
Francesco <71893290+FrancescoCode1@users.noreply.github.com>
2021-04-06 17:51:49 +0200

    [Minor]Cleaned code/Added admin commands

--- bot.py
+++ bot.py
@@ -1,18 +1,8 @@
 # bot.py
 # bot created by Francesco L.C. in association with nkaaf
-import os
+
 import discord
-import discord
-from discord.ext import commands
-import threading
 import os
-"""
-from random import shuffle, choice
-from cogs.utils.dataIO import dataIO
-from cogs.utils import checks
-from cogs.utils.chat_formatting import pagify, escape
-from urllib.parse import urlparse
-from __main__ import settings"""
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -26,7 +16,5 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client.load_extension("cogs.MusicCommands")
 client.load_extension("cogs.CatCommands")
-#client.load_extension("cogs.musix")
+client.load_extension("cogs.adminCommands")
 client.run(TOKEN)
-
-
